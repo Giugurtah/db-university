c684e17ba993b93d43270874774e20df04d475bb
Giugurtah <c.buoncompagni.98@gmail.com>
2021-09-28 17:12:31 +0200

    Es8 SELECT

--- quesries.sql
+++ quesries.sql
@@ -31,3 +31,8 @@ WHERE `level` = "magistrale";
 -- Da quanti dipartimenti è composta l'università?
 SELECT COUNT(*) 
 FROM `departments`;
+
+-- Quanti sono gli insegnanti che non hanno un numero di telefono?
+SELECT COUNT(*) 
+FROM `teachers`
+WHERE `phone` IS NULL;

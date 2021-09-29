87f42a36cfcd00ae9bd66ba7a93768cdb61d1184
Giugurtah <c.buoncompagni.98@gmail.com>
2021-09-29 15:02:33 +0200

    Es7 JOIN

--- queries2.sql
+++ queries2.sql
@@ -10,7 +10,7 @@ SELECT `degrees`.`id`, `degrees`.`name`, `degrees`.`level`, `departments`.`name`
 FROM `degrees`
 INNER JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
-WHERE `departments`.`name` = "Dipartimento di Neuroscienze"
+WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
 
 -- Selezionare tutti i corsi in cui insegna Fulvio Amato 
 SELECT `T`.`name`, `T`.`surname`, `C`.`name`   
@@ -19,7 +19,7 @@ INNER JOIN `teachers` `T`
 ON `CT`.`teacher_id` = `T`.`id`
 INNER JOIN `courses` `C`
 ON `CT`.`course_id` = `C`.`id`
-WHERE `teacher_id` = 44
+WHERE `teacher_id` = 44;
 
 -- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 SELECT 
@@ -33,7 +33,7 @@ INNER JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
 INNER JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
-ORDER BY `students`.`surname`, `students`.`name`
+ORDER BY `students`.`surname`, `students`.`name`;
 
 -- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 SELECT 
@@ -48,7 +48,7 @@ INNER JOIN `teachers` `T`
 ON `T`.`id` = `CT`.`teacher_id`
 INNER JOIN `degrees` `D`
 ON `D`.`id` = `C`.`degree_id`
-ORDER BY `D`.`name`
+ORDER BY `D`.`name`;
 
 -- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
 SELECT 
@@ -66,5 +66,20 @@ INNER JOIN `departments`
 ON `departments`.`id` = `degrees`.`id`
 WHERE `departments`.`name` = "Dipartimento di Matematica"
 GROUP BY `teachers`.`id`
-ORDER BY `teachers`.`surname`
+ORDER BY `teachers`.`surname`;
 
+-- Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT 
+`S`.`name` AS `student_name`,
+`S`.`surname` AS `student_surname`,
+`C`.`name` AS `exam_name`,
+COUNT(*) AS `attempts`
+FROM `exam_student` `ES`
+INNER JOIN `exams` `E`
+ON `E`.`id` = `ES`.`exam_id`
+INNER JOIN `students` `S`
+ON `S`.`id` = `ES`.`student_id`
+INNER JOIN `courses` `C`
+ON `C`.`id` = `E`.`course_id`  
+GROUP BY `C`.`id`, `S`.`id`
+ORDER BY `S`.`surname`, `S`.`name`;

e9f5e287dc46c304680ba85cb4909ddeee1351a4
Giugurtah <c.buoncompagni.98@gmail.com>
2021-09-29 14:06:25 +0200

    Es2 JOIN

--- queries2.sql
+++ queries2.sql
@@ -4,3 +4,10 @@ FROM `students` `S`
 INNER JOIN `degrees` `D`
 ON `D`.`id` = `S`.`degree_id`
 WHERE `D`.`name` = "Corso di Laurea in Economia";
+
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`id`, `degrees`.`name`, `degrees`.`level`, `departments`.`name`
+FROM `degrees`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Neuroscienze"
